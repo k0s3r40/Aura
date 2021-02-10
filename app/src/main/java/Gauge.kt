3d40218a5019e7b4d6cd98230b235995fb9282b8
dayoffsofia <slavov17@gmail.com>
2021-02-11 01:26:40 +0200

    WIP create Gauge class

---
+++ app/src/main/java/Gauge.kt
@@ -0,0 +1,12 @@
+class Gauge constructor(name: String,
+                        red_value: Float,
+                        orange_value: Float,
+                        yellow_value: Float,
+                        green_value: Float,
+                        min_value: Float,
+                        max_value: Float,
+                        current_value: Float) {
+
+
+
+}
