f8dc3c498df1b64c3b2edb5a2c7c7f395a7c60a9
dayoffsofia <slavov17@gmail.com>
2021-02-12 00:24:15 +0200

    WIp add gauge class

--- app/src/main/java/Gauge.kt
+++ app/src/main/java/Gauge.kt
@@ -1,12 +1,19 @@
-class Gauge constructor(name: String,
-                        red_value: Float,
-                        orange_value: Float,
-                        yellow_value: Float,
-                        green_value: Float,
-                        min_value: Float,
-                        max_value: Float,
-                        current_value: Float) {
+class Gauge constructor(var gauge_name: String,
+                        var red_value: Float,
+                        var orange_value: Float,
+                        var yellow_value: Float,
+                        var green_value: Float,
+                        var current_value: Float,
+                        val min_value: Float,
+                        val max_value: Float) {
+    val min_rotation: Int = 0;
+    val max_rotation: Int = 300;
+    var ratio  = max_value/max_rotation;
 
+    fun calculate_ranges() {
+
+
+    }
 
 
 }
