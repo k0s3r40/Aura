fb0e1cd50ae5eb1c0e56ad74a6a2bd58c1efddd6
dayoffsofia <slavov17@gmail.com>
2021-02-14 00:55:00 +0200

    Reduce the code big chunks

--- app/src/main/java/com/slavov17/aura/Gauge.kt
+++ app/src/main/java/com/slavov17/aura/Gauge.kt
@@ -1,8 +1,12 @@
-class Gauge(var gauge_name: String,
-            var red_value: ClosedRange<Float>,
-            var orange_value: ClosedRange<Float>,
-            var yellow_value: ClosedRange<Float>,
-            var green_value: ClosedRange<Float>,
+import android.widget.ImageView
+
+class Gauge(val arrow: ImageView,
+            val background: ImageView,
+            val gauge_name: String,
+            val red_value: ClosedRange<Float>,
+            val orange_value: ClosedRange<Float>,
+            val yellow_value: ClosedRange<Float>,
+            val green_value: ClosedRange<Float>,
             var current_value: Float,
             val min_value: Float,
             val max_value: Float) {
@@ -12,13 +16,22 @@ class Gauge(var gauge_name: String,
     val gauge_orange = 0xFFF5A947.toInt()
     val gauge_red = 0xFFD53E4F.toInt()
 
+    val base_rotation = -150;
+
     val min_rotation: Int = 0;
     val max_rotation: Int = 300;
     var ratio = max_rotation / max_value;
 
-    fun calculate_rotation(): Int {
-        return (current_value * ratio).toInt()
+    fun calculate_rotation(): Float{
+        return base_rotation+(current_value * ratio)
     }
+
+    fun rotate_gauge(){
+        arrow.rotation = calculate_rotation()
+        arrow.setColorFilter(get_color());
+        background.setColorFilter(get_color());
+    }
+
 
     fun get_color(): Int {
         when (current_value) {
--- app/src/main/java/com/slavov17/aura/ui/home/HomeFragment.kt
+++ app/src/main/java/com/slavov17/aura/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -31,6 +30,8 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         val eco_2_gauge = Gauge(
+                arrow =  root.findViewById(R.id.eco2_arrow_canvas),
+                background = root.findViewById(R.id.eco2_gauge_back),
                 gauge_name = "eCO2",
                 red_value = 2000F..5000F,
                 orange_value = 1000F..1999F,
@@ -41,6 +42,8 @@ class HomeFragment : Fragment() {
                 max_value = 5000F)
 
         val voc_gauge = Gauge(
+                arrow =  root.findViewById(R.id.voc_arrow_canvas),
+                background = root.findViewById(R.id.voc_gauge_back),
                 gauge_name = "VOC",
                 red_value = 1F..3F,
                 orange_value = 0.5F..0.99F,
@@ -51,6 +54,8 @@ class HomeFragment : Fragment() {
                 max_value = 3F)
 
         val humidity_gauge = Gauge(
+                arrow =  root.findViewById(R.id.hum_arrow_canvas),
+                background = root.findViewById(R.id.hum_gauge_back),
                 gauge_name = "Humidity",
                 red_value =60F..100F,
                 orange_value = 51F..59F,
@@ -61,6 +66,8 @@ class HomeFragment : Fragment() {
                 max_value = 100F)
 
         val pressure_gauge = Gauge(
+                arrow =  root.findViewById(R.id.pressure_arrow_canvas),
+                background = root.findViewById(R.id.pressure_gauge_back),
                 gauge_name = "BP",
                 red_value =2001F..3000F,
                 orange_value = 1501F..2000F,
@@ -71,88 +78,20 @@ class HomeFragment : Fragment() {
                 max_value = 3000F)
 
         Thread {
-            val eco_2_arrow_canvas: ImageView = root.findViewById(R.id.eco2_arrow_canvas)
-            val eco_2_gauge_back: ImageView = root.findViewById(R.id.eco2_gauge_back)
 
-            val voc_arrow_canvas: ImageView = root.findViewById(R.id.voc_arrow_canvas)
-            val voc_gauge_back: ImageView = root.findViewById(R.id.voc_gauge_back)
-
-            val hum_arrow_canvas: ImageView = root.findViewById(R.id.hum_arrow_canvas)
-            val hum_gauge_back: ImageView = root.findViewById(R.id.hum_gauge_back)
-
-            val pressure_arrow_canvas: ImageView = root.findViewById(R.id.pressure_arrow_canvas)
-            val pressure_gauge_back: ImageView = root.findViewById(R.id.pressure_gauge_back)
-
-
-
-            for (i in 1..300) {
-                eco_2_arrow_canvas.setColorFilter(get_colors(i));
-                eco_2_gauge_back.setColorFilter(get_colors(i));
-                eco_2_arrow_canvas.rotation = -150 + i.toFloat()
-
-                voc_arrow_canvas.setColorFilter(get_colors(i));
-                voc_gauge_back.setColorFilter(get_colors(i));
-                voc_arrow_canvas.rotation = -150 + i.toFloat()
-
-                hum_arrow_canvas.setColorFilter(get_colors(i));
-                hum_gauge_back.setColorFilter(get_colors(i));
-                hum_arrow_canvas.rotation = -150 + i.toFloat()
-
-                pressure_arrow_canvas.setColorFilter(get_colors(i));
-                pressure_gauge_back.setColorFilter(get_colors(i));
-                pressure_arrow_canvas.rotation = -150 + i.toFloat()
-
-                Thread.sleep(3)
-            }
-
-            for (i in 300 downTo 1) {
-                eco_2_arrow_canvas.rotation = -150 + i.toFloat()
-                eco_2_arrow_canvas.setColorFilter(get_colors(i));
-                eco_2_gauge_back.setColorFilter(get_colors(i));
-
-                voc_arrow_canvas.setColorFilter(get_colors(i));
-                voc_gauge_back.setColorFilter(get_colors(i));
-                voc_arrow_canvas.rotation = -150 + i.toFloat()
-
-                hum_arrow_canvas.setColorFilter(get_colors(i));
-                hum_gauge_back.setColorFilter(get_colors(i));
-                hum_arrow_canvas.rotation = -150 + i.toFloat()
-
-                pressure_arrow_canvas.setColorFilter(get_colors(i));
-                pressure_gauge_back.setColorFilter(get_colors(i));
-                pressure_arrow_canvas.rotation = -150 + i.toFloat()
-
-                Thread.sleep(1)
-
-            }
+            eco_2_gauge.current_value = 3000F
+            eco_2_gauge.rotate_gauge()
+            voc_gauge.current_value = 1F
+            voc_gauge.rotate_gauge()
+            humidity_gauge.current_value = 30F
+            humidity_gauge.rotate_gauge()
+            pressure_gauge.current_value = 1000F
+            pressure_gauge.rotate_gauge()
 
         }.start()
 
         return root
     }
 
-    fun get_colors(value: Int): Int {
 
-        var gauge_green = 0xFF66C2A5.toInt()
-        var gauge_yellow = 0xFFFDD448.toInt()
-        var gauge_orange = 0xFFF5A947.toInt()
-        var gauge_red = 0xFFD53E4F.toInt()
-        var transparent = 0xFFFDD448.toInt()
-        if (value >= 120) {
-            return gauge_red
-        }
-        if (value > 60) {
-            return gauge_orange
-        }
-
-        if (value > 16) {
-            return gauge_yellow
-        }
-
-        if (value > 0) {
-            return gauge_green
-        }
-
-        return R.color.gauge_red
-    }
 }
