bbbb087dc9af5f9e845a4d1e78524b0ee5ac0ea9
Kostadin Slavov <kostadin.slavov@tixi.bg>
2021-02-15 22:16:57 +0200

    Refactor bluetooth fragment

--- app/src/main/java/com/slavov17/aura/ui/bluetooth/BluetoothViewModel.kt
+++ app/src/main/java/com/slavov17/aura/ui/bluetooth/BluetoothViewModel.kt
@@ -1,13 +1,13 @@
-package com.slavov17.aura.ui.gallery
+package com.slavov17.aura.ui.bluetooth
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class BluetoothViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is bluetooth Fragment"
     }
     val text: LiveData<String> = _text
 }
